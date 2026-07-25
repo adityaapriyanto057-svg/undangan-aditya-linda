cb21f1b3c73ff19850aa33b3cd106b88fde3661b
adityaapriyanto057-svg <adityaapriyanto057@gmail.com>
2026-07-26 03:22:16 +0700

    Create script.js
    Menambahkan fungsi tombol undangan

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const button = document.querySelector("button");
+
+
+    button.addEventListener("click", function(){
+
+        alert("Selamat datang di Undangan Pernikahan Aditya & Linda ❤️");
+
+
+    });
+
+
+});
