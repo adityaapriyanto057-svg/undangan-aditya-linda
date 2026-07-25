f8893ecb463ff5a741ac158edf6c4cf2d46fb682
adityaapriyanto057-svg <adityaapriyanto057@gmail.com>
2026-07-26 03:44:08 +0700

    Menambahkan data undangan
    Menambahkan data undangan

---
+++ config.js
@@ -0,0 +1,34 @@
+const wedding = {
+
+    groom: {
+        name: "Aditya",
+        parents: "Putra dari Bapak Adi Rahman & Ibu Nurhasanah",
+        photo: "images/pria.jpg"
+    },
+
+
+    bride: {
+        name: "Linda",
+        parents: "Putri dari Bapak Ali & Ibu Atmani",
+        photo: "images/wanita.jpg"
+    },
+
+
+    event: {
+        date: "Rabu, 05 Agustus 2026",
+        time: "15.00 WIB - Selesai",
+        place: "Kediaman Bapak Adi Rahman Sekeluarga",
+        address: "Dusun Karpenang, Desa Manding Laok, Kecamatan Manding"
+    },
+
+
+    gift: {
+        dana: "+62 819-3093-1851"
+    },
+
+
+    guest:
+    "Bapak/Ibu/Saudara/i"
+
+
+};
